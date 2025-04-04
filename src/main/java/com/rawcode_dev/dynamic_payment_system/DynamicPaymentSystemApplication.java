344047a1844c79d579af635e12292db50fe8242c
Bibhuti Bhusan Pani <bapu@Bibhutis-MacBook-Air.local>
2025-04-04 17:05:16 +0530

    Basic spring boot application added

---
+++ src/main/java/com/rawcode_dev/dynamic_payment_system/DynamicPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rawcode_dev.dynamic_payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicPaymentSystemApplication.class, args);
+	}
+
+}
